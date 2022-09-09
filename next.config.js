9ec4664db81bf34585637c1e13640ad8ac09566b
rsshonjoydas <rsshonjoydas@gmail.com>
2022-09-10 05:09:18 +0600

    ci: project init

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    loader: 'akamai',
+    path: '/',
+    domains: [
+      'res.cloudinary.com',
+      'lh3.googleusercontent.com',
+      'firebasestorage.googleapis.com',
+      'graph.facebook.com',
+    ],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
+
+module.exports = nextConfig;

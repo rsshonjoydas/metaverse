a3ebc2bd84a00ec834c75038e44fe87e49d1e60e
”rsshonjoydas” <rsshonjoydas@email.com>
2023-01-26 20:11:41 +0600

    ci: about secton

--- components/CustomTexts.tsx
+++ components/CustomTexts.tsx
@@ -1,9 +1,27 @@
+/* eslint-disable react/no-array-index-key */
+
 'use client';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from 'framer-motion';
+import React from 'react';
+import { textContainer, textVariant2 } from '../utils/motion';
+
+interface Props {
+  title: string;
+  textStyle: string;
+}
+
+export const TypingText: React.FC<Props> = ({ title, textStyle }) => (
+  <motion.p
+    variants={textContainer}
+    className={`${textStyle} font-normal text-[14px] text-secondary-100 `}
+  >
+    {Array.from(title).map((letter, index) => (
+      <motion.span variants={textVariant2} key={index}>
+        {letter === '' ? '\u00A0' : letter}
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = () => <h2>Title Text</h2>;

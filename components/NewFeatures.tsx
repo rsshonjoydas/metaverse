ff8e5277669ac7460ed2c37aa2e4ed7cec55b420
”rsshonjoydas” <rsshonjoydas@email.com>
2023-01-27 07:22:46 +0600

    ci: new features secton

--- components/NewFeatures.tsx
+++ components/NewFeatures.tsx
@@ -1,6 +1,23 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+'use client';
+
+import { motion } from 'framer-motion';
+import styles from '../styles';
+
+interface NewFeatureProps {
+  imgUrl: string;
+  title: string;
+  subtitle: string;
+}
+
+const NewFeatures = ({ imgUrl, title, subtitle }: NewFeatureProps) => (
+  <div className='flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]'>
+    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}>
+      <motion.img src={imgUrl} alt='icon' className='object-contain w-1/2 h-1/2' />
+    </div>
+    <h1 className='mt-[26px] font-bold text-[24px] leading-[30.24px] text-white'>Title {title}</h1>
+    <p className='flex-1 mt-[16px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]'>
+      {subtitle}
+    </p>
   </div>
 );
 
